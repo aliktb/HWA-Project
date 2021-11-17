7416b514f035c1336dd317d53966dfb037be0a7b
aliktb <ali.khattab1@pm.me>
2021-11-17 14:27:30 +0000

    HWA-4 #comment added integration test for book controller

--- Code/HWAProject/src/main/resources/book-data.sql
+++ Code/HWAProject/src/main/resources/book-data.sql
@@ -1,4 +1,4 @@
 INSERT INTO `book`
-	(`author_last_name`, `author_first_name`, `book_title`, `checked_out`)
+	(`author_last_name`, `author_first_name`, `book_title`, `checked_out`, `customer_id`)
 values 
-	('Shakespeare', 'William', 'Hamlet', false);
+	('Shakespeare', 'William', 'Hamlet', false, null);
